0ee2f662a8e9c057d12c341a19b4855b935ed5ad
Matt Pedersen <mpedersen2054@gmail.com>
2014-12-10 20:39:24 -0600

    add ajax request to imgur user account and parse the results of a certain album of the user

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
-    
 
+    var clientId = 'dec355921049a2b';
+    $.ajaxSetup({
+        headers: {
+            'Authorization' : 'Client-ID ' + clientId
+        }
+    });
 
+    $.getJSON('https://api.imgur.com/3/account/patmedersen/album/6ZEqg/title',
+        function(results) {
+            console.log(results)
+            manipulateData(results.data.images);
+        }
+    );
 
 
 });
