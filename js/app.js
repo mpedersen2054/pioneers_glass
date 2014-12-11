fc6fee9a0b6fef95906af0d415a3c73afedcfd57
Matt Pedersen <mpedersen2054@gmail.com>
2014-12-11 03:30:12 -0600

    alot of tinkering and add near complete frontend

--- js/app.js
+++ js/app.js
@@ -1,69 +1,73 @@
 $(document).ready(function() {
-    var $this = $(this);
 
-    var clientId = 'dec355921049a2b';
-    $.ajaxSetup({
-        headers: {
-            'Authorization' : 'Client-ID ' + clientId
-        }
-    });
+var clientId = 'dec355921049a2b';
+$.ajaxSetup({
+    headers: {
+        'Authorization' : 'Client-ID ' + clientId
+    }
+});
 
-    $.getJSON('https://api.imgur.com/3/account/patmedersen/album/6ZEqg/title',
-        function(results) {
-            manipulateData(results.data.images);
-        }
-    );
+$.getJSON('https://api.imgur.com/3/account/patmedersen/album/6ZEqg/title',
+    function(results) {
+        manipulateData(results.data.images);
+    }
+);
 
-    function manipulateData(data) {
+function manipulateData(data) {
 
-        var alphaData = sortByTitle(data);
-        insertHtml(alphaData);
+    var alphaData = sortByTitle(data);
+    insertHtml(alphaData);
 
-        function sortByTitle(unsorted) {
-            var byTitle = unsorted.slice(0);
-            byTitle.sort(function(a, b) {
-                var x = a.title.toLowerCase(),
-                    y = b.title.toLowerCase();
+    function sortByTitle(unsorted) {
+        var byTitle = unsorted.slice(0);
+        byTitle.sort(function(a, b) {
+            var x = a.title.toLowerCase(),
+                y = b.title.toLowerCase();
 
-                if (x < y) { return -1 }
-                else if (x > y) { return 1 }
-                else { byTitle.slice(a,b) }
-            });
-            return byTitle
-        }
-
-        function sortByDate(unsorted) {
-            var byDate = unsorted.slice(0);
-            byDate.sort(function(a, b) {
-                return a.title - b.title
-            });
-        }
+            if (x < y) { return -1 }
+            else if (x > y) { return 1 }
+            else { byTitle.slice(a,b) }
+        });
+        return byTitle
     }
 
-    function insertHtml(items) {
+    function sortByDate(unsorted) {
+        var byDate = unsorted.slice(0);
+        byDate.sort(function(a, b) {
+            return a.title - b.title
+        });
     }
+}
+
+function insertHtml(items) {
+}
 
 
+// App Animations
 
-
-
-    // App Animations
-    
-    $('.artwork-box').hover(
-        function() {
-            $(this).find($('.artwork-meta'))
-                .css({
-                    'background-color': 'rgba(0,0,0,0.2)',
-                    'height': '200px'
-                });
-        },
-        function() {
-            $(this).find($('.artwork-meta'))
-                .css({
-                    'background-color': 'rgba(0,0,0,0.4)',
-                    'height': '36px'
-                });
-        }
-    );
+$('.artwork-image').hover(
+    function() {
+        $(this)
+            .find($('img'))
+                .addClass('blur')
+            .end()
+            .find($('.artwork-meta'))
+                .animate({
+                    height: '+=164px',
+                    fontSize: '1em',
+                }, 200);
+    },
+    function() {
+        $(this)
+            .find($('img'))
+                .removeClass('blur')
+            .end()
+            .find($('.artwork-meta'))
+                .animate({
+                    height: '-=164px',
+                    fontSize: '1em',
+                }, 300);
+    }
+);
 
 });
